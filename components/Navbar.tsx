be7e12108be3a458639df663befedeaea11bb7ef
Chungu Muloshi <148175759+Chungu13@users.noreply.github.com>
2026-02-10 23:41:50 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -40,7 +40,7 @@ export default function Navbar() {
                 </div>
 
                 <a
-                    href="cv.pdf"
+                    href="Chungu Muloshi Curriculum Vitae.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
