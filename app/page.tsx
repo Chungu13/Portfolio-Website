8f5b3fb225400864841e5d9ec5faa229cbedf5fe
Chungu Muloshi <148175759+Chungu13@users.noreply.github.com>
2026-02-10 21:57:15 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         "Backend system that enables manufacturers to apply for product certification online and allows consumers to verify certifications via QR codes.",
       tags: ["Python", "Django", "GraphQL", "PostgreSQL", "Docker"],
       github: "https://github.com/Chungu13/Chungu-Yamfumu-ZABS-Project",
-      image: "/zabsimage.png",
+      image: "/zabs.png",
     },
 
     {
